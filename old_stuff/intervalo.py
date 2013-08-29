37b0e060d6dcef0e41f6fec0ddaf43c9a779107c
David Davalos <davis_dg@hotmail.com>
2013-08-28 20:01:08 -0500

    Arreglado lo de old stuff

---
+++ old_stuff/intervalo.py
@@ -0,0 +1,24 @@
+class Intervalo(object):
+    def __init__(self,min,max=None):
+        if max is None:
+            max=min
+            
+        self.min=min
+        self.max=max
+    
+    #def __repr__(self):
+        #return 'Intervalo(%s,%s)' %(self.min,self.max)
+        #Otra manera:
+        #return 'Intervalo({},{})'.format(self.min,self.max)
+    #def __str__(self):
+        #return '[{},{}]'.format(self.min,self.max)
+    #Para el Ipython
+    #def _repr_html_(self):
+        #return '[{},{}]'.format(self.min,self.max)
+    #En LaTeX
+    def _repr_latex_(self):
+        return '$[{},{}]$'.format(self.min,self.max)
+    
+    def __add__(self,otro):
+        
+        return Intervalo(self.min+otro.min,self.max+otro.max)
