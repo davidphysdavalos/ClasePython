27709636aea4ce58172c4cd66f027510fb7be6d3
David Davalos <davidwarro@gmail.com>
2013-11-24 19:04:40 -0600

    Se sube a git tools para complex intervals

---
+++ ProyectoFinal/complex_interval_tools.py
@@ -0,0 +1,25 @@
+import cpintervalo as cpi
+import cintervalo as ci
+
+
+def polar2cart(x):
+    
+    if not isinstance(x, cpi.CPIntervalo):
+        
+        print 'Esto no es un intervalo en coordenadas polares'
+        
+        return None
+    
+    return ci.CIntervalo(x.mod*cpi.cos(x.arg), x.mod*cpi.sin(x.arg))
+    
+def cart2polar(x):
+    
+    if not isinstance(x, ci.CIntervalo):
+        
+        print 'Esto no es un intervalo en coordenadas cartesianas'
+        
+        return None
+    
+    return cpi.CPIntervalo(ci.abs(x), x.arg())
+    
+    
